253e9397d6dda94d4a8bdd8ea1fe4bc53eedb227
Cody Quinn <codyquinn1122@gmail.com>
2020-11-13 22:26:10 -0600

    :sparkles: Added 5 new songs

--- src/main/java/me/codyq/extradiscs/registries/Items.java
+++ src/main/java/me/codyq/extradiscs/registries/Items.java
@@ -11,7 +11,13 @@ import net.minecraft.util.registry.Registry;
 
 public class Items {
 
+    public static final Item DISC_0x10C = register("music_disc_0x10c", Sounds.SOUND_0x10C);
     public static final Item DISC_CLIFFSIDE_HINSON = register("music_disc_cliffside_hinson", Sounds.SOUND_CLIFFSIDE_HINSON);
+    public static final Item DISC_I_JATE_MY_HOB = register("music_disc_i_jate_my_hob", Sounds.SOUND_I_JATE_MY_HOB);
+    public static final Item DISC_PEANUTS = register("music_disc_peanuts", Sounds.SOUND_PEANUTS);
+    public static final Item DISC_REPETITION = register("music_disc_repetition", Sounds.SOUND_REPETITION);
+    public static final Item DISC_SOMETIMES_I_MAKE_VIDEO_GAME_MUSIC = register("music_disc_sometimes_i_make_video_game_music",
+            Sounds.SOUND_SOMETIMES_I_MAKE_VIDEO_GAME_MUSIC);
 
     public static Item register(String id, SoundEvent sound) {
         Item.Settings settings = new Item.Settings().rarity(Rarity.RARE).maxCount(1).group(ItemGroup.MISC);
--- src/main/java/me/codyq/extradiscs/registries/Sounds.java
+++ src/main/java/me/codyq/extradiscs/registries/Sounds.java
@@ -7,7 +7,12 @@ import net.minecraft.util.registry.Registry;
 
 public class Sounds {
 
+    public static final SoundEvent SOUND_0x10C = register("music_disc.0x10c");
     public static final SoundEvent SOUND_CLIFFSIDE_HINSON = register("music_disc.cliffside_hinson");
+    public static final SoundEvent SOUND_I_JATE_MY_HOB = register("music_disc.i_jate_my_hob");
+    public static final SoundEvent SOUND_PEANUTS = register("music_disc.peanuts");
+    public static final SoundEvent SOUND_REPETITION = register("music_disc.repetition");
+    public static final SoundEvent SOUND_SOMETIMES_I_MAKE_VIDEO_GAME_MUSIC = register("music_disc.sometimes_i_make_video_game_music");
 
     public static SoundEvent register(String name) {
         Identifier id = new Identifier(ExtraDiscs.MOD_ID, name);
